47335e86119438c35d67d37f6744de4855794b4b
nebullii <nchaudhari@clarku.edu>
2026-03-09 20:05:04 -0400

    Skip omniauth validation during rake tasks (assets:precompile)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,7 @@ google_secret = ENV["GOOGLE_CLIENT_SECRET"].presence || Rails.application.creden
 
 # Validate at server boot (not during asset precompile / rake tasks)
 Rails.application.config.after_initialize do
+  next if defined?(Rake)  # skip during assets:precompile and other rake tasks
   if Rails.env.production? || Rails.env.development?
     raise "GITHUB_CLIENT_ID is not set"     if ENV["GITHUB_CLIENT_ID"].blank?
     raise "GITHUB_CLIENT_SECRET is not set" if ENV["GITHUB_CLIENT_SECRET"].blank?
